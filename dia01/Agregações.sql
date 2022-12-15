d9833a0e39e0b71179fe61b0d409a4a4a06b6a0e
Rafilzisk <Rafilzisk>
2022-12-15 00:33:04 +0000

    Agregrações

--- dia01/Agregações.sql
+++ dia01/Agregações.sql
@@ -40,3 +40,35 @@ SELECT
 
 
 FROM silver_olist.cliente
+
+-- COMMAND ----------
+
+SELECT 
+  COUNT(*) AS qtLinhas,
+  COUNT (distinct idCliente) AS qtClientes,
+  COUNT (distinct idClienteUnico) AS qtClientesUnicos
+  
+ FROM silver_olist.cliente
+WHERE descCidade IN ('presidente prudente','curitiba')
+
+
+
+
+-- COMMAND ----------
+
+SELECT
+      AVG(vlPreco) AS avgPreco, --- media da coluna vlPreco (preco médio dos produtos)
+      
+      PERCENTILE(vlPreco, 0.5) AS medianPreco, --- preço mediano
+      
+      
+      MAX(vlPreco) AS maxPreco,
+      AVG(vlFrete) AS avgFrete,
+      MAX(vlFrete) AS vlFrete,
+      MIN(vlFrete) AS vlFrete
+      
+
+FROM silver_olist.item_pedido
+
+
+
